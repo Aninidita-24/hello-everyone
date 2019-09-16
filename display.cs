ac06725312f44a93de401a1a4bdd6ac7ff015286
Aninidita-24 <55387407+Aninidita-24@users.noreply.github.com>
2019-09-16 17:12:50 +0530

    Create display.cs

---
+++ display.cs
@@ -0,0 +1,14 @@
+using system;
+namespace lists
+{
+  public class program{
+   static void Main(string[] args){
+   float value = 5 * 4 - 12;
+            float value1 = value / 10;
+            Console.WriteLine(value);
+            Console.WriteLine(value1);
+            decimal v = 1/3m;
+            Console.WriteLine(v);
+          }
+         }
+       }  
